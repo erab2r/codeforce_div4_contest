d049e119c05521c004d8392ca89ec4bdeed6aaff
erab2r <shkera213@gmail.com>
2025-12-27 22:42:17 +0600

    1

--- p1.cpp
+++ p1.cpp
@@ -5,20 +5,32 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        long long e;
-        cin >> e;
-        while (e % 2 == 0) {
-            e /= 2;
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<long long> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
         }
 
-        if (e == 1 || e == 3)
-            cout << "Yes\n";
-        else
-            cout << "No\n";
+        long long total = 0;
+        for (long long x : a) total += x;
+
+        long long ans = LLONG_MIN;
+        long long pref = 0;
+
+        for (int k = 0; k < n; k++) {
+            // k is the remaining child
+            long long left = pref;
+            long long right = total - pref - a[k];
+            ans = max(ans, left - right);
+            pref += a[k];
+        }
+
+        cout << ans << "\n";
     }
-    
     return 0;
 }
